fd3c92d77fe391671df66b56248f516f092a7a11
hyp0515 <b102030019@g-mail.nsysu.edu.tw>
2024-03-04 16:42:41 +0800

    Backup

--- mcmc_test.py
+++ mcmc_test.py
@@ -7,14 +7,18 @@ import corner
 def T_model(theta):
     a_max, Mstar = theta
     opacity_table=disk_model.generate_opacity_table(
-        a_min=0,a_max=a_max,q=-3.5,dust_to_gas=0.01)
+        a_min=0,a_max=a_max,q=-3.5,dust_to_gas=0.01
+        )
     disk_property_table = disk_model.generate_disk_property_table(
-        opacity_table=opacity_table)
+        opacity_table=opacity_table
+        )
     DM = disk_model.DiskModel(
-        opacity_table=opacity_table, disk_property_table=disk_property_table)
+        opacity_table=opacity_table, disk_property_table=disk_property_table
+        )
     DM.generate_disk_profile(
         Mstar=Mstar*disk_model.Msun, Mdot=Mstar*1e-5*disk_model.Msun/disk_model.yr,
-        Rd=30*disk_model.au,Q=1.5,N_R=50)
+        Rd=30*disk_model.au,Q=1.5,N_R=50
+        )
     R_grid = DM.R[1:]/disk_model.au
     T_mid = DM.T_mid
     return R_grid, T_mid
@@ -42,8 +46,9 @@ def log_probability(theta, y, yerr):
         return -np.inf
     return lp + log_likelihood(theta, y, yerr)
 
+
 nwalkers, ndim = 32, 2  # Number of walkers and dimension of the parameter space
-pos = [np.array([0.1, 0.14]) + 1e-4 * np.random.randn(ndim) for i in range(nwalkers)]
+pos = [np.array([0.1, 0.14]) + 1e-7 * np.random.randn(ndim) for i in range(nwalkers)]
 
 sampler = emcee.EnsembleSampler(nwalkers, ndim, log_probability, args=(t_mid_train, yerr))
-sampler.run_mcmc(pos, 5000, progress=True)
+sampler.run_mcmc(pos, 100, progress=True)
